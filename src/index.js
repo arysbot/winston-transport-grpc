97947771ce47192067a804f80677191049d64c2f
torsina <theo.sagot.1@gmail.com>
2018-11-21 18:34:22 +0100

    server address is now a parameter when invoking the class, wrote "test"

--- src/index.js
+++ src/index.js
@@ -9,30 +9,40 @@ module.exports = class Grpc extends Transport {
     constructor(opts) {
         super(opts);
 
-        this.level = {};
-        if (Array.isArray(opts.level)) {
-            for (let level in opts.level) {
-                this.level[level] = 1;
-            }
-        } else {
-            this.level.push(opts.level);
-        }
-        this.service = opts.service;
+        this.serverURL = opts.serverURL || "";
+        this.config = opts.config || {};
+        this.level = opts.level;
         this._grpc = {};
         this._grpc.path = path.join(__dirname, "../node_modules/protofiles/src/Logger.proto");
         this._grpc.definition = protoLoader.loadSync(this._grpc.path);
         this._grpc.object = grpc.loadPackageDefinition(this._grpc.definition);
-        this.Logger = new this._grpc.object.Logger(`dns:///logger.arys-${NODE_ENV}.svc.cluster.local`, {
-            "grpc.lb_policy_name": "round_robin"
-        });
+        this.Logger = new this._grpc.object.Logger(this.serverURL, grpc.credentials.createInsecure(), this.config);
     }
 
     log(info, callback) {
         setImmediate(() => {
             this.emit("logged", info);
         });
-        if(this.level[info.level]) {
+        if(info.level === this.level) {
+            const request = {
+                uuid: info.uuid,
+                service: info.service,
+                request: info.request,
+                code: info.code,
+                metadata: info.metadata
+            };
+            if (!request.uuid) throw new Error("Request must provide a uuid.\n" + request);
+            if (!request.service) throw new Error("Request must provide its service.\n" + request);
+            if (!request.request) throw new Error("Request must provide it's type.\n" + request);
+            if (!request.code) throw new Error("Request must provide it's response code.\n" + request);
+            if (request.metadata && typeof request.metadata !== "string") throw new Error("Request metadata must be a JSON string.\n" + request);
+            this.Logger.Log(request, (error, res) => {
+                if (error) {
+                    return console.log('An error occurred:', error);
+                }
 
+                console.log('Response:', res);
+            })
         }
 
         // Perform the writing to the remote service
--- tests/index.js
+++ tests/index.js
@@ -9,6 +9,16 @@ const logger = winston.createLogger({
         // - Write to all logs with level `info` and below to `combined.log`
         // - Write all logs error (and below) to `error.log`.
         //
-        new Grpc({ filename: 'error.log', level: 'error' }),
+        new Grpc({ level: "error",
+            serverURL: "127.0.0.1:8881"
+        })
     ]
+});
+
+logger.log({
+    level: "error",
+    uuid: "dfht",
+    service: "tester",
+    request: "log",
+    code: "200"
 });
